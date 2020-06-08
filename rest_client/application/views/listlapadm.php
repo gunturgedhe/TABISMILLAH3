8ad22bb0cb1c9e359ad9de440af7ba706757db57
dwiananda <dwianandairhama@gmail.com>
2020-06-08 19:23:17 +0700

    baru

--- rest_client/application/views/listlapadm.php
+++ rest_client/application/views/listlapadm.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://pmw.etupolinema.org/assets/css/style.css">
 </head>
 <body style="background-image:url(http://pmw.etupolinema.org/assets/img/bg5.png)">
 <nav class="navbar navbar-default">
